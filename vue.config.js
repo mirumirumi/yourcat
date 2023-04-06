66e448e558b6255c265528802ace43e3aea5af48
mirumi <mail@mirumi.tech>
2023-04-06 18:24:14 +0900

    fix: issues of updating

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
       },
     },
   },
-  publicPath: process.env.NODE_ENV === "production"
+  publicPath: process.env.NODE_ENV === "production" || process.env.NODE_ENV === "development"
     ? "/apps/yourcat/"
     : "/",
 };
